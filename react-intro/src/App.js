13d5f31fa7a7db1de759eff4374aca093dd199d0
Ethan Adams <ethan.adams747@gmail.com>
2022-02-23 01:47:51 -0500

    Learning props

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -2,13 +2,21 @@ import React, {useState} from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import Nick from "./img/Nick.jpeg"
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <Card 
+        person={Nick}
+        rating="5.0"
+        reviewCount={6}
+        location="Arizona"
+        title="Life skills with Nick"
+        price={199}
+      />
     </div>
   )
 }
--- react-intro/src/components/Card.js
+++ react-intro/src/components/Card.js
@@ -1,19 +1,18 @@
 import React from "react";
-import person from "../img/Nick.jpeg"
 import star from "../img/star.png"
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div className="card">
-            <img src={person} alt="Nick Cage" className="card--img" />
+            <img src={props.person} alt="Nick Cage" className="card--img" />
             <div className="card--stats">
                 <img src={star} alt="star" className="card--star"/>
-                <span className="card--rating">5.0</span>
-                <span className="gray">(6) •</span>
-                <span className="gray"> Arizona</span>
+                <span className="card--rating">{props.rating}</span>
+                <span className="gray">({props.reviewCount}) •</span>
+                <span className="gray">{props.location}</span>
             </div>
-            <p className="card--title">Life lessons with Nick</p>
-            <p className="card--cost"><span className="bold">From $189 </span> / person</p>
+            <p className="card--title">{props.title}</p>
+            <p className="card--cost"><span className="bold">From ${props.price} </span> / person</p>
         </div>
     )
 }
